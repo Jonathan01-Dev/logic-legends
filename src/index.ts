8b3916f37e45aac933ce26eed5d8884d77553d4b
Erwin K. OKE <okekomerwin@gmail.com>
2026-02-28 02:23:47 +0000

    refactor(core): nettoyage du code pour test sur machines physiques

--- src/index.ts
+++ src/index.ts
@@ -6,34 +6,27 @@ import { TcpServer } from './network/tcpServer';
 
 const keyFile = path.join(process.cwd(), 'keys', 'node_identity.json');
 
-// 1. Sécurité : On vérifie si les clés existent avant de parser
+// 1. Sécurité : On vérifie si les clés existent avant de démarrer
 if (!fs.existsSync(keyFile)) {
     console.error('Clés introuvables ! Lance d\'abord : node src/crypto/generateKeys.js');
     process.exit(1);
 }
 
-// 2. Chargement de l'identité
+// 2. Chargement de l'identité cryptographique PURE
 const identity = JSON.parse(fs.readFileSync(keyFile, 'utf-8'));
-const baseNodeId = Buffer.from(identity.ed25519.publicKey, 'hex'); // 32 bytes
+const nodeId = Buffer.from(identity.ed25519.publicKey, 'hex'); // 32 bytes intacts
+const nodeIdHex = nodeId.toString('hex').substring(0, 8); // Pour l'affichage
 
-// 3. Récupération du port TCP via CLI (ex: npx tsx src/index.ts 7777)
+// 3. Récupération du port TCP via CLI (Défaut : 7777)
 const args = process.argv.slice(2);
 const portArg = args.find(arg => !isNaN(parseInt(arg)));
 const tcpPort = portArg ? parseInt(portArg) : 7777;
-
-// -------------------------------------------------------------
-// HACK HACKATHON LOCAL : Pour que les 3 terminaux sur le même PC aient des ID différents
-// On remplace les 2 derniers octets de la clé publique par le numéro de port TCP.
-baseNodeId.writeUInt16BE(tcpPort, 30);
-const nodeId = baseNodeId;
-// -------------------------------------------------------------
-
-const nodeIdHex = nodeId.toString('hex').substring(0, 8); // Pour l'affichage
 
 console.log(`\n🚀 Démarrage du Nœud Archipel [ID: ${nodeIdHex}...]`);
 console.log(`--------------------------------------------------`);
 
 // 4. Lancement du Serveur TCP (Module 1.3)
+// On passe le nodeId pur pour forger les paquets Handshake
 const tcpServer = new TcpServer(nodeId, tcpPort);
 tcpServer.start();
 
@@ -42,4 +35,4 @@ const discovery = new Discovery(nodeId, tcpPort);
 discovery.start();
 
 console.log(`--------------------------------------------------`);
-console.log(`Nœud opérationnel. En attente de pairs...\n`);
+console.log(`Nœud opérationnel. En attente de pairs sur le réseau local...\n`);
